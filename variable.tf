4d1eb0579864e2cebf4413287445d6f9ec97b0fa
Armand Janse van Rensburg <armandjvr1991@gmail.com>
2024-09-26 17:07:29 +0400

    created rds file

--- variable.tf
+++ variable.tf
@@ -47,3 +47,28 @@ variable "ssh_location" {
   description   = "the ip address that can ssh into the ec2 instances"
   type          = string
 }
+
+# rds variables
+variable "database_snapshot_identifier" {
+  default       = "arn:aws:rds:us-east-1:905418252000:snapshot:a4lwordpress-with-cat-post-mysql-8032" 
+  description   = "database snapshot arn"
+  type          = string
+}
+
+variable "database_instance_class" {
+  default       = "db.t2.micro" 
+  description   = "the database instance type"
+  type          = string
+}
+
+variable "database_instance_identifier" {
+  default       = "rdsmultiazsnap-db-0jlun6obkdwi" 
+  description   = "the database instance identifier"
+  type          = string
+}
+
+variable "multi_az_deployment" {
+  default       = false 
+  description   = "create a standby db instance"
+  type          = bool
+}
